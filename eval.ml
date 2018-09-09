3df329c0422cafa70b6a7e3d4812a93c71642e1c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-09-09 19:45:39 -0400

    Some style improvements

--- eval.ml
+++ eval.ml
@@ -6,13 +6,14 @@ type 'a eval_result =
   | Success of 'a
   | Error of string
 
-let eval (env : Sxp.t Env.t) expr =
-  let rec lookup_name s =
-    Option.map_default (fun x -> Success x) (Error "no such variable")
-      (env#get_var s)
-  and _eval_args args0 = match args0 with
+let lookup_name (env : Sxp.t Env.t) s =
+  Option.map_default (fun x -> Success x) (Error "no such variable")
+    (env#get_var s)
+
+let rec eval (env : Sxp.t Env.t) expr =
+  let rec _eval_args args0 = match args0 with
     | [] -> Success []
-    | (y :: ys) -> match _eval y with
+    | (y :: ys) -> match eval env y with
                    | Error e -> Error e
                    | Success z ->
                       match _eval_args ys with
@@ -28,7 +29,7 @@ let eval (env : Sxp.t Env.t) expr =
       end
   and function_call x = match x with
     | (y :: ys, Nil) -> begin
-        match _eval y with
+        match eval env y with
         | Success f -> begin
             match _eval_args ys with
             | Success args -> begin
@@ -41,14 +42,13 @@ let eval (env : Sxp.t Env.t) expr =
         | Error e -> Error e
       end
     | _ -> Error "malformed function call"
-  and _eval expr = match expr with
-    | Symbol s -> lookup_name s
-    | Cons _ -> function_call (SList.to_list expr)
-    | x -> Success x
   and _eval_body exprs = match exprs with
     | [] -> Success Nil
-    | e :: [] -> _eval e
-    | e :: es -> match _eval e with
+    | e :: [] -> eval env e
+    | e :: es -> match eval env e with
                  | Success _ -> _eval_body es
                  | Error x -> Error x
-  in _eval expr
+  in match expr with
+     | Symbol s -> lookup_name env s
+     | Cons _ -> function_call (SList.to_list expr)
+     | x -> Success x
--- eval.mli
+++ eval.mli
@@ -4,3 +4,8 @@ type 'a eval_result =
   | Error of string
 
 val eval : Sxp.t Env.t -> Sxp.t -> Sxp.t eval_result
+(*
+val eval_each : Sxp.t Env.t -> Sxp.t list -> Sxp.t list eval_result
+
+val eval_seq : Sxp.t Env.t -> Sxp.t list -> Sxp.t eval_result
+ *)
